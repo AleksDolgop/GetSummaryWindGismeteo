a58fe8238178596196201afe838f32d719da7acd
Aleksandr <dolopolov9@gmail.com>
2021-04-07 03:51:13 +0700

    feat: chart.js

--- script.js
+++ script.js
@@ -6,13 +6,94 @@
 // @author       AleksDolgop
 // @match         https://www.gismeteo.ru/diary/*/*/*/
 // @grant        none
+// @require https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js
 // ==/UserScript==
-// repo link https://github.com/AleksDolgop/GetSummaryWindGismeteo/blob/main/script.js
 
-// https://www.gismeteo.ru/diary/205920/2019/12/
+// https://www.chartjs.org/docs/latest/getting-started/usage.html
+// https://www.chartjs.org/docs/latest/getting-started/usage.html
 
 let lastReadedData = null;
 let preData = null;
+
+function allSum(...args) {
+  let sum = 0;
+  for (const arg of args) {
+    sum += arg;
+  }
+  return sum;
+}
+
+function repaintWindRose() {
+  const oldRose = document.getElementById('tempRoseDataBlock');
+  if (oldRose) {
+    document.removeChild(oldRose);
+  }
+  const roseDiv = document.createElement('div');
+  roseDiv.id = 'tempRoseDataBlock';
+  roseDiv.style.position = 'fixed';
+  roseDiv.style.bottom = 0;
+  roseDiv.style.right = 0;
+  roseDiv.style.backgroundColor = '#fff';
+  roseDiv.style.border = '1px solid black';
+  roseDiv.style.height = '200px';
+  roseDiv.style.width = '200px';
+  roseDiv.style.zIndex = 1000;
+  roseDiv.style.marginBottom = '10px';
+  roseDiv.style.marginRight = '10px';
+
+  const newCanvas = document.createElement('canvas');
+  newCanvas.id = 'tempWindRoseGraph';
+  newCanvas.style.width = '200px';
+  newCanvas.style.height = '200px';
+
+  let massData = Object.keys(lastReadedData).map(key => lastReadedData[key]);
+  const sum = allSum(...massData);
+  massData = massData.map(item => Math.round((item * 100) / sum));
+  console.log(massData)
+  var myRadarChart = new window.Chart(newCanvas, {
+    type: 'radar',
+    data: {
+      labels: Object.keys(lastReadedData),
+      datasets: [
+        {
+          label: '',
+          data: massData,
+          fill: true,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgb(255, 99, 132)',
+          pointBackgroundColor: 'rgb(255, 99, 132)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgb(255, 99, 132)',
+        },
+      ],
+    },
+    options: {
+      responsiveAnimationDuration: 2000,
+      maintainAspectRatio: false,
+      pointDot: false,
+      pointLabelFontSize: 0,
+      tooltip: {
+        enabled: false,
+      },
+      legend: {
+        position: 'bottom',
+        display: false,
+      },
+      pointLabel: {
+        fontSize: 0,
+      },
+      // scale: {
+      //   ticks: {
+      //     display: false,
+      //   },
+      // },
+    },
+  });
+
+  roseDiv.appendChild(newCanvas);
+  document.body.appendChild(roseDiv);
+}
 
 function copyToClipboard(str) {
   var area = document.createElement('textarea');
@@ -154,4 +235,6 @@ function addStorage() {
 
   printPreData();
   document.body.appendChild(newDiv);
+
+  repaintWindRose();
 })();
